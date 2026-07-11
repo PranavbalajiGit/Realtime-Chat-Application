8a9a4197e60c8e705a6db1acdb2bfdb7723f934a
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-07-11 23:54:28 +0530

    Implement Clerk webhook handling and middleware setup

---
+++ backend/src/webhooks/clerk.webhook.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import User from "../models/user.model.js";
+import {verifyWebhook} from "@clerk/backend/webhooks"
+
+const router = express.Router()
+
+router.post("/" , async(req,res) => {
+    const signingSecret = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+    if(!signingSecret) {
+        res.status(503).json({message:"Webhook secret is not provided"});
+        return;
+    }
+});
+
+export default router
