c6406521156dfc8a6f09605f1af379cfb34a5bb3
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-08-09 12:31:08 +0530

    Implement authentication middleware and update checkAuth route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+export async function checkAuth(req, res , next) {
+    if(!req.user) return res.status(401).json({message:"Unauthorized"});
 
-
-export async function checkAuth(req, res , next) {}
+    res.status(200).json(req.user);
+}
