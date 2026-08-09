8e0869e6b923879f3b0bfa4a3d8896779af9cf68
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-08-09 12:20:50 +0530

    Add authentication routes and checkAuth controller

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,9 @@
+
+import express from "express";
+import { checkAuth } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/check" , checkAuth)
+
+export default router;
