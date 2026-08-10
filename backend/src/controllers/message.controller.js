32ca455d3195387ddbbe1f74fe6724dbcbb6f366
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-08-10 23:22:05 +0530

    Add getMessages controller and update message routes

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -65,3 +65,23 @@ export async function getConversationsForSidebar(req, res) {
         res.status(500).json({ message: "Internal server error" });
     }
 }
+
+export async function getMessages(req, res) {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+        $or: [
+            { senderId: myId, receiverId: userToChatId },
+            { senderId: userToChatId, receiverId: myId },
+        ],
+        }).sort({ createdAt: 1 });
+
+        res.status(200).json(messages);
+    } 
+    catch (error) {
+        console.error("Error in getMessages:", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
