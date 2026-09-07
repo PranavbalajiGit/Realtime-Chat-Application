76733e9b7a06ac705a13cf24d6ea885596562731
PranavbalajiGit <pranavbalaji65690@gmail.com>
2026-09-07 23:46:39 +0530

    feat: enhance ChatPage component with chat store integration and user management

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
+import { useWallpaper } from "../context/wallpaper.js";
+import { useChatStore } from "../store/useChatStore.js";
+import { useSelectedConversation } from "../hooks/useSelectedConversation";
+import { useEffect } from "react";
+
 
 function ChatPage() {
+  const {frameStyle} = useWallpaper()
+
+  const getConversations = useChatStore((state) => state.getConversations);
+  const getMessages = useChatStore((state) => state.getMessages);
+  const getUsers = useChatStore((state) => state.getUsers);
+  const subscribeToMessages = useChatStore((state) => state.subscribeToMessages);
+  const unsubscribeFromMessages = useChatStore((state) => state.unsubscribeFromMessages);
+
+  const { activeConversation, activeConversationId, isLargeScreen } = useSelectedConversation();
+
+  useEffect(() => {
+    getUsers();
+    getConversations();
+  }, [getConversations, getUsers]);
+
+  useEffect(() => {
+    if (!activeConversationId) return;
+
+    getMessages(activeConversationId);
+    subscribeToMessages(activeConversationId);
+
+    // cleanup
+    return () => unsubscribeFromMessages();
+  }, [getMessages, activeConversationId, subscribeToMessages, unsubscribeFromMessages]);
+  
   return (
     <div>ChatPage</div>
   )
